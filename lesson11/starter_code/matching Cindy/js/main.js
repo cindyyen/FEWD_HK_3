36e4ea2d44ea6fb3f1747ca81ec00f0d9d291580
Cindy Yen <cindyyen47@gmail.com>
2013-11-25 19:18:14 +0800

    pulling lesson 13

---
+++ lesson11/starter_code/matching Cindy/js/main.js
@@ -0,0 +1,20 @@
+//on start game - shuffle cards
+	//shuffle cards:
+		//
+
+
+//play #1-1 click on 1st choice, card stay visible
+$("").click(function(){
+
+
+})
+
+
+//play #1-2 click on 2nd card
+
+
+//evaluate play, if match both cards stay visible, record a match and an "attempt"
+//if mismatch both cards flip back, record an "attempt"
+
+
+//Matched all - count attempts, alert
