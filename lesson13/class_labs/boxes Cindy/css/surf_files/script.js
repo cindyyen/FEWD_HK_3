a9ed39ce3bbc3e22c4e2f32880031b046fdf6b2a
Cindy Yen <cindyyen47@gmail.com>
2013-12-02 19:37:24 +0800

    yo

---
+++ lesson13/class_labs/boxes Cindy/css/surf_files/script.js
@@ -0,0 +1,14 @@
+jQuery(function($){
+  	$('section#twitter p').tweet({
+	            username: "dannpetty",
+	            join_text: "auto",
+	            avatar_size: 0,
+	            count: 1,
+	            auto_join_text_default: "",
+	            auto_join_text_ed: "",
+	            auto_join_text_ing: "",
+	            auto_join_text_reply: "",
+	            auto_join_text_url: "",
+	            loading_text: ""
+			});
+});
